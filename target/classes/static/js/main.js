ed56afb3d491cf4b18400c5181aafc4c64e574ac
Mike Lamprakis <mikelamprakis@Mikes-MacBook-Pro.local>
2025-09-16 23:58:18 +0100

    update

--- target/classes/static/js/main.js
+++
@@ -1,4 +0,0 @@
-import { initialize } from './core/initialization.js'; // Import initialization logic
-
-// Start the application when the DOM is loaded
-document.addEventListener('DOMContentLoaded', initialize); 
